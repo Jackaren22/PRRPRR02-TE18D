341e9215ca928aff96927a58555f2d85620d70a3
Jackaren22 <Jackaren22@gmail.com>
2021-01-22 14:51:19 +0100

    i did it
    i think i did it, i maybe did not do it, but i believe that i have done it

--- SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
+++ SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
@@ -6,30 +6,49 @@ namespace SOLID_Labb
     {
         static void Main(string[] args)
         {
-            Animal[] animals = {new Dog("Black"),
-                            new Hedgehog("Pink"),
-                            new Bird("Yellow")};
+            Animal[] animals = AnimalColor();
 
             Console.WriteLine("###############");
             Console.WriteLine();
 
+            Loop(animals);
+
+
+
+
+        }
+
+        static Animal[] AnimalColor()
+        {
+            Animal[] animals = {new Dog("Black"),
+                            new Hedgehog("Pink"),
+                            new Bird("Yellow")};
+            return animals;
+        }
+
+        static void Loop(Animal[] animals)
+        {
             foreach (var animal in animals)
             {
                 if (animal is Dog dog)
                 {
                     dog.TransferOwnership("Niklas");
                 }
-
-                animal.Eat();
-                animal.Sleep();
-                animal.Speak();
-                animal._color = "Grey";
-                Console.WriteLine(animal.GetType().Name + " is " + animal._color);
-                Console.WriteLine();
-
-                Console.WriteLine("###############");
-                Console.WriteLine();
+                AnimalText(animal);
             }
+        }
+
+        static void AnimalText(Animal animal)
+        {
+            animal.Eat();
+            animal.Sleep();
+            animal.Speak();
+            animal._color = "Grey";
+            Console.WriteLine(animal.GetType().Name + " is " + animal._color);
+            Console.WriteLine();
+
+            Console.WriteLine("###############");
+            Console.WriteLine();
         }
     }
 }
